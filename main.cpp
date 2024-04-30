fc10edebb1ae57cdef94b6c5752851fa41b85b1f
ZkillerZJT <87343938+ZkillerZJT@users.noreply.github.com>
2024-04-30 20:17:24 +0000

    v0.1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 using namespace std;
-
+int tipoINT=0;
 class Local{//ta weno, quizás agregar int id;
 private:
     int superficie;
@@ -11,43 +11,62 @@ private:
     float monto_arriendo;
     string nombre_local;
 public:
-    Local(int superficie_, string tipo_local_, string nombre_local_) {//constructor
+    Local(){}
+    void Local2(int superficie_, string tipo_local_, string nombre_local_) {//constructor
         superficie = superficie_;
         tipo_local = tipo_local_;
         string nombre_local = nombre_local_;
-        monto_arriendo = calcularArriendo(superficie, tipo_local);
+        //monto_arriendo = calcularArriendo(superficie, tipoINT);
     }
     //al indicar superficie en m2 del local y su nombre, se debe especificar el tipo de local antes de usar el constructor.
     ~Local() {}
         //definir setters
-
-
-
+    void setSuperficie(int superficie_){
+        this->superficie=superficie_;
+    }
+    /*void setTipolocal(string tipo_local_){ // no necesario en este caso, placeholder.
+        this->tipo_local=tipo_local_;
+    }*/
+    void setTipolocal(int tipoINT){ // falta la logica para utilizar esta función y utilizar la variable
+        if (tipoINT==1)         // int usado en el menu de creacion de objeto
+            this->tipo_local="oficina";
+        if (tipoINT==2)
+            this->tipo_local="local comercial";
+    }
+    void setNombrelocal(string nombre_local_){
+        this->nombre_local=nombre_local_;
+    }
+    void setMontoarriendo(int superficie_){
+        this->superficie=superficie_;
+    }
         //definir getters
-
-
+    int getsuperficie(){
+        return superficie;
+    }
+    string getTipolocal(){
+        return this->tipo_local;
+    }
+    string getNombrelocal(){
+        return this->nombre_local;
+    }
+    float getMontoarriendo(){
+        return this->monto_arriendo;
+    }
         //definir visualizador de objetos
-    float calcularArriendo(int superficie, string tipo_local) { // ta weno
+
+};
+float calcularArriendo(int superficie, int tipoINT) { // funcion miembro
         // 0.5 uf/m2 para oficinas, 0.65 uf/m2 para locales comerciales
         int precio_calculado=0;
-        if(tipo_local=="oficina"){
+        if(tipoINT=1){// oficina
             precio_calculado=superficie*0,5;
         }
-        else if (tipo_local=="local comercial"){
+        else if (tipoINT=2){// local comercial
             precio_calculado=superficie*0,65;
         }
         return precio_calculado;
     }
-    void setTipo(int tipoINT){ // falta la logica para utilizar esta función y utilizar la variable
-        if (tipoINT==1)         // int usado en el menu de creacion de objeto
-            tipo_local="oficina";
-        if (tipoINT==1)
-            tipo_local="local comercial";
-    }
-};
-
 void mostrarMenu() { //ta weno
-    int opcion=0;
     cout << "=== Menú ===" << endl;
     cout << "1. Crear objeto" << endl;
     cout << "2. Ver listado de objetos" << endl;
@@ -57,7 +76,7 @@ void mostrarMenu() { //ta weno
 void HandleChoice(int opcion){// falta logica 
     switch (opcion){
         case 1:
-                // llamado a funcion de crearobjeto
+            crearLocal();    // llamado a funcion de crearobjeto
             break;
         case 2:
                 //llamado a funcion ver listado de objetos
@@ -67,14 +86,38 @@ void HandleChoice(int opcion){// falta logica
             break;
         case 0:
         exit(0);// puede que este bien o no que esto se encuentre aquí, hay una condicion en main para case 0.
-        /*case default:
+        default:
             cout<<"opcion incorrecta, intente nuevamente."<<endl;
             main();
-            break; */
+            break;
         
     }
 }
+void checktipoINT(){
+if(tipoINT>2 || tipoINT<1){
+        cout<<"Opción indicada es incorrecta, usted digito "<<tipoINT<<" y se esperaba 1 o 2."<<endl;
+        cout<<"indique tipo de local digitando 1 para oficina y 2 para local comercial:"<<endl;
+    cin>>tipoINT;
+    checktipoINT();
+    }
+}
 void crearLocal(){//falta toda la logica usando setters y usar las variables.
+    cout<<"indique nombre de local:"<<endl;
+    string locnombre;
+    cin>>locnombre;
+    cout<<"indique tipo de local digitando 1 para oficina y 2 para local comercial:"<<endl;
+    cin>>tipoINT;
+    checktipoINT();
+    cout<<"indique superficie de local en m2:"<<endl;
+    int locsuperficie;
+    cin>>locsuperficie;
+    Local L;
+    L.setNombrelocal(locnombre);
+    L.setTipolocal(tipoINT);
+    L.setSuperficie(locsuperficie);
+    L.setMontoarriendo(calcularArriendo(locsuperficie, tipoINT));
+
+
 
     }
 
